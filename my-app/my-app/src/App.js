4f372d6b4b79009dafa68c4e97addaeff9e86e36
Anton K. <krivelo2017@mail.ru>
2024-03-31 20:41:37 +0300

    react lesson 19 add  router-dom

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -4,17 +4,23 @@ import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Dialogs from './components/Dialogs/Dialogs';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-        <Header />
-        <Nav />
-        {/* <Profile /> */}
-        <Dialogs />
-    </div>
+    <BrowserRouter >
+        <div className="app-wrapper">
+            <Header />
+            <Nav />
+
+
+            <Routes>
+              <Route path="/profile" element={<Profile />}/>
+              <Route path="/dialogs" element={<Dialogs />}/>
+            </Routes>
+        </div>
+    </BrowserRouter>
   );
 } 
 
--- my-app/my-app/src/components/Nav/Nav.jsx
+++ my-app/my-app/src/components/Nav/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = () => {
     return (
         <nav className={classes.nav}>
             
-            <div><a className={classes.item}><i class="fi fi-rr-user"></i>Profile</a></div>
-            <div><a className={classes.item}><i class="fi fi-rr-envelope"></i>Messages</a></div> 
+            <div><a href="/profile" className={classes.item}><i class="fi fi-rr-user"></i>Profile</a></div>
+            <div><a href="/dialogs" className={classes.item}><i class="fi fi-rr-envelope"></i>Messages</a></div> 
             <div><a className={classes.item}><i class="fi fi-rr-calendar"></i>News</a></div> 
             <div><a className={classes.item}><i class="fi fi-rr-megaphone"></i>Music</a></div> 
             <div><a className={classes.item}><i class="fi fi-rr-settings"></i>Settings</a></div>
