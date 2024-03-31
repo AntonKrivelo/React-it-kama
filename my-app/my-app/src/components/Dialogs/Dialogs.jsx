beeb44a426ac49e2f900cd16c63d5932b3a8485e
Anton K. <krivelo2017@mail.ru>
2024-03-31 19:31:41 +0300

    add pack react-router-dom install

--- my-app/my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/my-app/src/components/Dialogs/Dialogs.jsx
@@ -13,10 +13,8 @@ const Dialogs = () => {
                         <img className={classes.avatar} src="https://avatars.mds.yandex.net/get-shedevrum/11511289/cc2855ebcbb411eea6ebbaaee90618f0/orig" />
                         <span className={classes.message}>Hi my friends</span>
                     </div>
-                </div>
-                
+                </div>  
         </div>
-        
     );
 }
 
