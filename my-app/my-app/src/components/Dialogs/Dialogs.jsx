377700b5561377e155a07b27608df22276c1de4f
Anton K. <krivelo2017@mail.ru>
2024-04-07 18:57:19 +0300

    add onClick textarea Dialogs Messages

--- my-app/my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/my-app/src/components/Dialogs/Dialogs.jsx
@@ -6,10 +6,24 @@ import Message from "./Message";
 
 const Dialogs = (props) => {
 
+
+
+    let newMessagesElement = React.createRef();
+
+    
+    let submitMessage = () => {
+        let textMessage = newMessagesElement.current.value;
+        alert(textMessage);
+    }
+
+
+
    
     let dialogsElements = props.state.dialogsData.map(d => <DialogItem name={d.name} id={d.id} /> );
     let messagesElements = props.state.messagesData.map(m => <Message message={m.message} id={m.id} /> );
    
+    
+
     return (
        <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
@@ -17,6 +31,10 @@ const Dialogs = (props) => {
             </div>
             <div className={classes.messages}>
                 {messagesElements}
+                <form className={classes.addMessages}>
+                    <textarea ref={newMessagesElement} className={classes.textAreaMessage} placeholder="transfereed message..."></textarea>
+                    <button onClick={submitMessage} className={classes.btnMessage}>Submit</button>
+                </form>
             </div>
        </div>
     );
